6f29f7644cc7198ab441d7fd20f296d80b512f58
Andrea Lai <alai.arpas@gmail.com>
2021-08-06 10:45:41 +0200

    esri-rest is working

--- packages/usa/src/index.ts
+++ packages/usa/src/index.ts
@@ -2,5 +2,5 @@ import {nome, due} from '@jalai/utilita';
 import {richiesta as coloriCarg} from '@jalai/esri-rest';
 
 console.log("Colore:")
-coloriCarg().then(v => {console.log(v.splice(1,1))})
+coloriCarg().then(v => {console.log(v.splice(1,5))})
 console.log(nome + due)
